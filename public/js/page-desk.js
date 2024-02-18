b9277ad69651e6f650c8135c3d366187c315a324
Divesh Adivarekar <100069544+diveshadivarekar@users.noreply.github.com>
2024-02-18 16:27:43 +0530

    update desk page

--- public/js/page-desk.js
+++ public/js/page-desk.js
@@ -36,13 +36,8 @@ document.addEventListener("DOMContentLoaded", () => {
                                   }
                                 </p>
                                 <div
-                                  class="iq-progress-bar bg-secondary-light mb-4"
+                                  class="border-top mb-4"
                                 >
-                                  <span
-                                    class="bg-secondary iq-progress progress-1"
-                                    data-percent="65"
-                                    style="transition: width 2s ease 0s; width: 100%"
-                                  ></span>
                                 </div>
                                 <div
                                   class="d-flex align-items-center justify-content-between"
@@ -110,4 +105,51 @@ function deleteFile(fileId) {
       console.error("Error deleting file:", error);
     });
   location.reload();
+}
+
+function addTask() {
+  // const status = document.getElementById("status").value || "open";
+  const taskName = document.getElementById("taskName").value;
+  const assignedTo = document.getElementById("assignedTo").value;
+  const dueDate = document.getElementById("dueDate").value;
+  const category = document.getElementById("category").value;
+  const project = document.getElementById("project").value;
+  const description = document.getElementById("description").value;
+  // const checklist = document
+  //   .getElementById("checklist")
+  //   .value.split(",")
+  //   .map((item) => item.trim());
+
+  // const attachments = document
+  //   .getElementById("attachments")
+  //   .value.split(",")
+  //   .map((item) => item.trim());
+
+  const taskData = {
+    status: "open",
+    taskName: taskName,
+    assignedTo: assignedTo,
+    dueDate: dueDate,
+    category: category,
+    project: project,
+    description: description,
+    checklist: "future feature",
+    // attachments: attachments,
+  };
+
+  // Send the task data to the server
+  fetch("/add-task", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(taskData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      alert(data.message);
+      // Redirect to the view_tasks.html page
+      window.location.href = "#";
+    })
+    .catch((error) => console.error("Error adding task:", error));
 }
